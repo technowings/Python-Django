3c909c1234cb8f9befc4446394e218c4fef5292c
unknown <gaikwad.anirudha@gmail.com>
2020-09-05 14:17:02 +0530

    second edit

--- Python_WorkPlace/Introduction_to_Python/PythonComment.py
+++ Python_WorkPlace/Introduction_to_Python/PythonComment.py
@@ -1,9 +1,8 @@
 # Comments in Python ::: its Single Line Comment
-""""
-Comments in Python ::: 
-its Single Line Comment
-"""
-
+# """
+# Comments in Python ::: 
+# its multi Line Comment
+# """
 
 x="Hello Python"
 print(x)
