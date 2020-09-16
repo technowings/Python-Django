45da287566d15a49c8252817b8740f23bd836041
unknown <gaikwad.anirudha@gmail.com>
2020-09-16 08:26:50 +0530

    6 commit

--- Python_WorkPlace/Python_File_Operations/CopyFiles.py
+++ Python_WorkPlace/Python_File_Operations/CopyFiles.py
@@ -1,19 +1,19 @@
 # Python Program - Copy Files
 
 from shutil import copyfile;
-print("Enter 'x' for exit.");
+print("Enter 'x' for exit.")
 sourcefile = input("Enter source file name (copy from): ");
 if sourcefile == 'x':
-    exit();
+    exit()
 else:
     destinationfile = input("Enter destination file name (copy to): ");
-    copyfile(sourcefile, destinationfile);
-    print("File copied successfully!");
-    print("Want to display the content ? (y/n): ");
-    check = input();
+    copyfile(sourcefile, destinationfile)
+    print("File copied successfully!")
+    print("Want to display the content ? (y/n): ")
+    check = input()
     if check == 'n':
-        exit();
+        exit()
     else:
-        c = open(destinationfile, "r");
-        print(c.read());
-        c.close();
+        c = open(destinationfile, "r")
+        print(c.read())
+        c.close()
--- Python_WorkPlace/Python_File_Operations/DeleteFile.py
+++ Python_WorkPlace/Python_File_Operations/DeleteFile.py
@@ -1,11 +1,11 @@
 # Python Program - Delete File
 
 import os;
-print("Enter 'x' for exit.");
-filename = input("Enter name of file to delete: ");
+print("Enter 'x' for exit.")
+filename = input("Enter name of file to delete: ")
 if filename == 'x':
-    exit();
+    exit()
 else:
-    print("\nRemoving the file....");
-    os.remove(filename);
-    print("\nFile,",filename,"successfully deleted!!");
+    print("\nRemoving the file....")
+    os.remove(filename)
+    print("\nFile,",filename,"successfully deleted!!")
