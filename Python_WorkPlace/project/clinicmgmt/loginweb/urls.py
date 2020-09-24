e3e4c62d43d357d80dfaa5fa2d30f2752da9de91
unknown <gaikwad.anirudha@gmail.com>
2020-09-24 19:50:13 +0530

    Remove duplicated directory

--- Python_WorkPlace/project/clinicmgmt/loginweb/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path,include
-from . import views
-
-urlpatterns=[
-    path('login',views.web_login,name='login'),
-    path('dashboard',views.web_dashboard,name='dashboard'),
-]
